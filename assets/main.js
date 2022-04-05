60f57b51668ecfd4e8ec295b15ba256458085796
iftikhar <ifti891@gmail.com>
2022-04-05 10:32:11 +0530

    working on calculator

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+// required abilities of a calculator
+//accept users inputs of number, operator and another number
+//should accpet decimal numbers
+//store inputs
+//recognise inputs and perform calculations
+//return results
+//optional featires:
+//should accept longer arthmetic operations
+//display the inputs as they being typed(inputed)
+//store previus total as start of next operations.
+//clear button should clear all entries
+//should prevent invalid inputs (operatores next to each other, two decimal points)
+
+const keys = document.querySelector(".calculator-buttons")
