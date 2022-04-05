804cdcfc1ff99f062b01766c0ad47e27a9762287
iftikhar <ifti891@gmail.com>
2022-04-05 12:26:42 +0530

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -11,4 +11,50 @@
 //clear button should clear all entries
 //should prevent invalid inputs (operatores next to each other, two decimal points)
 
-const keys = document.querySelector(".calculator-buttons")
+const keys = document.querySelector(".calculator-buttons");
+keys.addEventListener("click", (event) => {
+  const { target } = event;
+  const { value } = target;
+
+  if (!target.matches("button")) {
+    return;
+  } else {
+    //pass to parse method
+    calculator.parseInput(value);
+    // console.log(target)
+  }
+});
+
+const calculator = {
+  displayText: "0",
+  prevTotal: null,
+
+  parseInput(value) {
+   
+
+    // have any of the "special buttons" been clicked
+    switch (value) {
+      case "=":
+        // calculate the answer
+        break;
+      case "AC":
+        //clear screen and stored value
+        break;
+      case ".":
+        if (this.displayText == 0) {
+          //pass '0.' into add text method
+        } else {
+          //add value to text string
+        }
+        break;
+      default:
+        //add value to text string
+        break;
+    }
+  },
+  addText(value) {
+    if (this.displayText === "0") {
+      this.displayText = " ";
+    }
+  },
+};
