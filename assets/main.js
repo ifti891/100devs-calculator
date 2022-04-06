7a39b8c22406c227fa035f795902d796628aa182
iftikhar <ifti891@gmail.com>
2022-04-06 14:43:58 +0530

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -30,31 +30,61 @@ const calculator = {
   prevTotal: null,
 
   parseInput(value) {
-   
-
     // have any of the "special buttons" been clicked
     switch (value) {
       case "=":
+        this.calcAnswer(this.displayText);
         // calculate the answer
         break;
       case "AC":
         //clear screen and stored value
+        this.clearAll();
         break;
       case ".":
         if (this.displayText == 0) {
           //pass '0.' into add text method
+          this.addText("0.");
         } else {
           //add value to text string
+          this.addText(value);
         }
         break;
       default:
         //add value to text string
+        this.addText(value);
         break;
     }
   },
   addText(value) {
     if (this.displayText === "0") {
       this.displayText = " ";
+    } else if (this.prevTotal !== null) {
+      this.displayText = this.prevTotal;
+      this.prevTotal = null;
     }
+    /* check weather the last char in display AND the entered value are not numbers*/
+    if (isNaN(+value) && isNaN(+this.displayText)) {
+      if (isNaN(this.displayText.slice(-1))) {
+        return;
+      }
+    }
+    this.displayText += value;
+    //output display text to screen
+    this.outputText(this.displayText);
+  },
+
+  outputText(text) {
+    document.querySelector(".calculator-screen").value = text;
+  },
+
+  calcAnswer(equation) {
+    let result = Function("return " + equation)();
+    this.outputText(result);
+  },
+
+  clearAll() {
+    (this.displayText = "0"),
+      (this.prevTotal = null),
+      this.outputText(this.displayText);
   },
 };
